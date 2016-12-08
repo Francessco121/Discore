f72f5b98503dec10ec6a9329a04553e27aa4e707
Tristan Smith <tristan@shortcord.com>
2016-12-07 19:14:47 -0500

    Cleaned up Webhooks
    Documented DiscordWebhook and DiscordHttpWebhookEndpoint

--- src/Discore/Http/DiscordHttpWebhookEndpoint.cs
+++ src/Discore/Http/DiscordHttpWebhookEndpoint.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using Discore.Http.Net;
+﻿using Discore.Http.Net;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Discore.Http
@@ -14,16 +15,51 @@ namespace Discore.Http
             this.endpoint = endpoint;
         }
 
+        /// <summary>
+        /// Create a <see cref="DiscordWebhook"/> on a <see cref="DiscordChannel"/>
+        /// </summary>
+        /// <param name="name">Webhook bot's username that shows in chat</param>
+        /// <param name="avatar">Webhook bot's avatar</param>
+        /// <param name="channel">The channel the webhook lives on</param>
+        /// <returns><see cref="DiscordWebhook"/></returns>
         public async Task<DiscordWebhook> CreateWebhook(string name, DiscordAvatarData avatar, DiscordChannel channel)
-            => new DiscordWebhook(await endpoint.CreateWebhook(name, avatar, channel.Id));
+            => await CreateWebhook(name, avatar, channel.Id);
 
+        /// <summary>
+        /// Create a <see cref="DiscordWebhook"/> using a <see cref="Snowflake"/> of a <see cref="DiscordChannel"/>
+        /// </summary>
+        /// <param name="name">Webhook bot's username that shows in chat</param>
+        /// <param name="avatar">Webhook bot's avatar</param>
+        /// <param name="Id">The channel the webhook lives on</param>
+        /// <returns><see cref="DiscordWebhook"/></returns>
+        public async Task<DiscordWebhook> CreateWebhook(string name, DiscordAvatarData avatar, Snowflake Id)
+            => new DiscordWebhook(await endpoint.CreateWebhook(name, avatar, Id));
+
+        /// <summary>
+        /// Get a <see cref="DiscordWebhook"/> via its <see cref="Snowflake"/>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns><see cref="DiscordWebhook"/></returns>
         public async Task<DiscordWebhook> GetWebhook(Snowflake id)
             => new DiscordWebhook(await endpoint.GetWebhook(id));
 
+        /// <summary>
+        /// Get a List of <see cref="DiscordWebhook"/> on a <see cref="DiscordChannel"/>
+        /// </summary>
+        /// <param name="channel"><see cref="DiscordChannel"/> to poll</param>
+        /// <returns><see cref="DiscordChannel"/>(s)</returns>
         public async Task<IReadOnlyList<DiscordWebhook>> GetWebhooks(DiscordChannel channel)
+            => await GetWebhooks(channel.Id);
+
+        /// <summary>
+        /// Get a List of <see cref="DiscordWebhook"/> on a <see cref="Snowflake"/>
+        /// </summary>
+        /// <param name="id"><see cref="Snowflake"/> of a <see cref="DiscordChannel"/> to poll</param>
+        /// <returns><see cref="IReadOnlyList{DiscordChannel}"/></returns>
+        public async Task<IReadOnlyList<DiscordWebhook>> GetWebhooks(Snowflake id)
         {
             List<DiscordWebhook> toReturn = new List<DiscordWebhook>();
-            DiscordApiData json = await endpoint.GetChannelWebhooks(channel.Id);
+            DiscordApiData json = await endpoint.GetChannelWebhooks(id);
             if (json.Type != DiscordApiDataType.Array)
                 throw new DiscoreException("Malformed json response");
 
@@ -33,17 +69,56 @@ namespace Discore.Http
             return toReturn;
         }
 
+        /// <summary>
+        /// Modify an exsting <see cref="DiscordWebhook"/>
+        /// </summary>
+        /// <param name="webhook"><see cref="DiscordWebhook"/> to modify</param>
+        /// <param name="name">Only updateed if not null</param>
+        /// <param name="avatar">Only updated if not null</param>
+        /// <returns><see cref="DiscordWebhook"/></returns>
         public async Task<DiscordWebhook> ModifyWebhook(DiscordWebhook webhook, string name = null, DiscordAvatarData avatar = null)
-            => new DiscordWebhook(await endpoint.ModifyWebhook(webhook.Id, name, avatar));
+            => await ModifyWebhook(webhook.Id, name, avatar);
 
-        public async Task DeleteWebhook(DiscordWebhook webhook, bool useToken)
+        /// <summary>
+        /// Modify an exsting <see cref="DiscordWebhook"/> via its <see cref="Snowflake"/>
+        /// </summary>
+        /// <param name="Id"><see cref="Snowflake"/> of the <see cref="DiscordWebhook"/> to modify</param>
+        /// <param name="name">Only updateed if not null</param>
+        /// <param name="avatar">Only updated if not null</param>
+        /// <returns><see cref="DiscordWebhook"/></returns>
+        public async Task<DiscordWebhook> ModifyWebhook(Snowflake Id, string name = null, DiscordAvatarData avatar = null)
+            => new DiscordWebhook(await endpoint.ModifyWebhook(Id, name, avatar));
+
+        /// <summary>
+        /// Delete an existing <see cref="DiscordWebhook"/>
+        /// </summary>
+        /// <param name="webhook"><see cref="DiscordWebhook"/> to delete</param>
+        public async Task DeleteWebhook(DiscordWebhook webhook)
         {
-            if (useToken)
-                await endpoint.DeleteWebhook(webhook.Id, webhook.Token);
+            if (webhook.HasToken) // if we have a token
+                await endpoint.DeleteWebhook(webhook.Id, webhook.Token); 
             else
                 await endpoint.DeleteWebhook(webhook.Id);
         }
 
+        /// <summary>
+        /// Delete an existing <see cref="DiscordWebhook"/> via its <see cref="Snowflake"/>
+        /// </summary>
+        /// <param name="Id"><see cref="Snowflake"/> of a <see cref="DiscordWebhook"/> to delete</param>
+        public async Task DeleteWebhook(Snowflake Id)
+        {
+            DiscordWebhook wh = await GetWebhook(Id);
+            await DeleteWebhook(wh);
+        }
+
+        /// <summary>
+        /// Execute a <see cref="DiscordWebhook"/> with a message
+        /// </summary>
+        /// <param name="webhook"><see cref="DiscordWebhook"/> to execute</param>
+        /// <param name="content">Message to post to the <see cref="DiscordChannel"/></param>
+        /// <param name="username">Temporally override <see cref="DiscordWebhook.Name"/></param>
+        /// <param name="avatar"><see cref="Uri"/> to an image, temporally override <see cref="DiscordWebhook.Avatar"/></param>
+        /// <param name="tts">Text to Speech</param>
         public async Task ExecuteWebhook(DiscordWebhook webhook,
             string content,
             string username = null,
@@ -51,6 +126,15 @@ namespace Discore.Http
             bool tts = false)
             => await endpoint.ExecuteWebhook(webhook.Id, webhook.Token, content, username, avatar, tts);
 
+        /// <summary>
+        /// Execute a <see cref="DiscordWebhook"/> with a file
+        /// </summary>
+        /// <param name="webhook"><see cref="DiscordWebhook"/> to execute</param>
+        /// <param name="file">File to post to the <see cref="DiscordChannel"/></param>
+        /// <param name="filename">Filename of the file, defaults to unknown.jpg</param>
+        /// <param name="username">Temporally override <see cref="DiscordWebhook.Name"/></param>
+        /// <param name="avatar"><see cref="Uri"/> to an image, temporally override <see cref="DiscordWebhook.Avatar"/></param>
+        /// <param name="tts">Text to Speech</param>
         public async Task ExecuteWebhook(DiscordWebhook webhook,
             byte[] file,
             string filename = "unknown.jpg",
@@ -59,16 +143,49 @@ namespace Discore.Http
             bool tts = false)
             => await endpoint.ExecuteWebhook(webhook.Id, webhook.Token, file, filename, username, avatar, tts);
 
-        //Need to setup a way to serialize DiscordEmbed to a json blob
+        /// <summary>
+        /// Execute a <see cref="DiscordWebhook"/> with a file
+        /// </summary>
+        /// <param name="webhook"><see cref="DiscordWebhook"/> to execute</param>
+        /// <param name="file">File to post to the <see cref="DiscordChannel"/></param>
+        /// <param name="username">Temporally override <see cref="DiscordWebhook.Name"/></param>
+        /// <param name="avatar"><see cref="Uri"/> to an image, temporally override <see cref="DiscordWebhook.Avatar"/></param>
+        /// <param name="tts">Text to Speech</param>
         public async Task ExecuteWebhook(DiscordWebhook webhook,
-            DiscordEmbed[] embeds,
+            FileInfo file,
             string username = null,
             Uri avatar = null,
             bool tts = false)
         {
-            throw new NotImplementedException();
 
-            //return await endpoint.ExecuteWebhook(webhook.Id, webhook.Token, embeds, username, avatar, tts);
+            using (FileStream fs = file.OpenRead())
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await fs.CopyToAsync(ms);
+                await endpoint.ExecuteWebhook(webhook.Id, webhook.Token, ms.ToArray(), file.Name, username, avatar, tts);
+            }
+
+        }
+
+        /// <summary>
+        /// Execute a <see cref="DiscordWebhook"/> with <see cref="DiscordEmbed"/>
+        /// </summary>
+        /// <param name="webhook"><see cref="DiscordWebhook"/> to execute</param>
+        /// <param name="embeds"><see cref="DiscordEmbed"/>(s) to post</param>
+        /// <param name="username">Temporally override <see cref="DiscordWebhook.Name"/></param>
+        /// <param name="avatar"><see cref="Uri"/> to an image, temporally override <see cref="DiscordWebhook.Avatar"/></param>
+        /// <param name="tts">Text to Speech</param>
+        public async Task ExecuteWebhook(DiscordWebhook webhook,
+            IEnumerable<DiscordEmbed> embeds,
+            string username = null,
+            Uri avatar = null,
+            bool tts = false)
+        {
+            DiscordApiData data = DiscordApiData.CreateArray();
+            foreach (DiscordEmbed embed in embeds)
+                data.Values.Add(embed.Serialize());
+
+            await endpoint.ExecuteWebhook(webhook.Id, webhook.Token, data, username, avatar, tts);
         }
     }
 }
--- src/Discore/Http/DiscordWebhook.cs
+++ src/Discore/Http/DiscordWebhook.cs
@@ -1,18 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Discore.Http
+﻿namespace Discore.Http
 {
-    public class DiscordWebhook : DiscordIdObject
+    public sealed class DiscordWebhook : DiscordIdObject
     {
+        /// <summary>
+        /// The Id of Guild this Webhook belongs to
+        /// </summary>
         public Snowflake Guild { get; }
+        /// <summary>
+        /// The Id of the Channel this Webhook was created on
+        /// </summary>
         public Snowflake Channel { get; }
+        /// <summary>
+        /// The User that created this Webhook
+        /// </summary>
         public DiscordUser User { get; }
+        /// <summary>
+        /// The public name of this Webhook
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// The Avatar of this Webhook
+        /// </summary>
         public DiscordAvatarData Avatar { get; }
+        /// <summary>
+        /// The token of this Webhook<para/>
+        /// This is only populated if the we created the webhook, otherwise its empty/null<para/>
+        /// Its used for Executing, Updating, and Deleting said webhook without the need of authorization<para/>
+        /// We as a bot user can't modify or execute a webhook that we don't own
+        /// </summary>
         public string Token { get; }
+        public bool HasToken { get { return !string.IsNullOrWhiteSpace(Token); } }
 
         public DiscordWebhook(DiscordApiData data)
             :base(data)
@@ -20,9 +37,10 @@ namespace Discore.Http
             Guild = data.GetSnowflake("guild_id").Value;
             Channel = data.GetSnowflake("channel_id").Value;
 
-            DiscordApiData tData;
-            if (DiscordApiData.TryParseJson(data.GetString("user"), out tData))
-                //User = new DiscordUser(tData);
+            DiscordApiData userData = data.Get("user");
+            if (!userData.IsNull)
+                User = new DiscordUser(userData);
+
 
             Name = data.GetString("name");
             Avatar = new DiscordAvatarData(data.GetString("avatar"));
