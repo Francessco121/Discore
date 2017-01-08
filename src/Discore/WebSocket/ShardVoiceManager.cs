25ec18ded741fd476ea850b9fc34aec663b01408
Ethan Lafrenais <elafrenais12@gmail.com>
2017-01-08 17:15:23 -0500

    ShardVoiceManager.CreateConnection now checks if the user can connect
    This is a safe-guard for commit 9325ecbc83174d7da02035c05d5e258516213351. CreateConnection will throw an exception if the user does not have permission to connect, or the channel is full and the user is not an administrator.

--- src/Discore/WebSocket/ShardVoiceManager.cs
+++ src/Discore/WebSocket/ShardVoiceManager.cs
@@ -45,6 +45,12 @@ namespace Discore.WebSocket
         /// If a voice connection already exists for the given channel,
         /// a new connection is not created and the existing one is returned.
         /// </summary>
+        /// <exception cref="DiscordPermissionException">
+        /// Thrown if the current user does not have permission to connect to the voice channel.
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the voice channel is full, and the current user does not have the administrator permission.
+        /// </exception>
         public DiscordVoiceConnection CreateConnection(DiscordGuildVoiceChannel voiceChannel)
         {
             DiscordVoiceConnection connection;
@@ -62,13 +68,31 @@ namespace Discore.WebSocket
                 DiscoreMemberCache memberCache;
                 if (guildCache.Members.TryGetValue(shard.User.Id, out memberCache))
                 {
-                    // Create the new connection
-                    connection = new DiscordVoiceConnection(shard, gateway, guildCache, memberCache, voiceChannel);
-                    if (voiceConnections.TryAdd(voiceChannel.GuildId, connection))
-                        return connection;
+                    // Check if the user has permission to connect.
+                    DiscordPermissionHelper.AssertPermission(DiscordPermission.Connect,
+                        memberCache.Value, guildCache.Value, voiceChannel);
+
+                    if (guildCache.VoiceChannels.TryGetValue(voiceChannel.Id, out DiscoreVoiceChannelCache voiceChannelCache))
+                    {
+                        bool isAdmin = DiscordPermissionHelper.HasPermission(DiscordPermission.Administrator,
+                            memberCache.Value, guildCache.Value, voiceChannel);
+
+                        if (isAdmin || voiceChannel.UserLimit == 0 || voiceChannelCache.ConnectedMembers.Count < voiceChannel.UserLimit)
+                        {
+                            // Create the new connection
+                            connection = new DiscordVoiceConnection(shard, gateway, guildCache, memberCache, voiceChannel);
+                            if (voiceConnections.TryAdd(voiceChannel.GuildId, connection))
+                                return connection;
+                            else
+                                // Connection already exists, just return the existing one.
+                                return voiceConnections[voiceChannel.GuildId];
+                        }
+                        else
+                            throw new InvalidOperationException(
+                                $"The voice channel is full, and the current authenticated user does not have the administrator permission.");
+                    }
                     else
-                        // Connection already exists, just return the existing one.
-                        return voiceConnections[voiceChannel.GuildId];
+                        throw new DiscoreCacheException("The specified voice channel does not exist in the local cache.");
                 }
                 else
                     // This really should never ever ever happen.
