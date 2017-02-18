080afc8d719ac4fb51fdc8fda090719d812e7524
Unknown <elafrenais12@gmail.com>
2017-02-18 17:56:00 -0500

    Version bump for 2.1.2

--- src/Discore/Properties/AssemblyInfo.cs
+++ src/Discore/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Bundled Sticks Inkorperated")]
 [assembly: AssemblyProduct("Discore")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyVersion("2.1.2")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
