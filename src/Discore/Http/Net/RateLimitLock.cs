467369ccbcd12b5c3f1f9a7427892d792e6c1255
Ethan Lafrenais <elafrenais12@gmail.com>
2017-06-04 15:44:57 -0400

    Simplify HTTP rate limiting into a single handler

---
+++ src/Discore/Http/Net/RateLimitLock.cs
@@ -0,0 +1,57 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Discore.Http.Net
+{
+    class RateLimitLock
+    {
+        public bool RequiresWait
+        {
+            get
+            {
+                TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+                ulong epochNowMilliseconds = (ulong)t.TotalMilliseconds;
+
+                int msDelay = (int)(resetAtEpochMilliseconds - epochNowMilliseconds);
+                return msDelay > 0;
+            }
+        }
+
+        ulong resetAtEpochMilliseconds;
+        AsyncLock mutex;
+
+        public RateLimitLock()
+        {
+            mutex = new AsyncLock();
+        }
+
+        public Task WaitAsync(CancellationToken cancellationToken)
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            ulong epochNowMilliseconds = (ulong)t.TotalMilliseconds;
+
+            int msDelay = (int)(resetAtEpochMilliseconds - epochNowMilliseconds);
+            return msDelay <= 0 ? Task.CompletedTask : Task.Delay(msDelay);
+        }
+
+        public void ResetAt(ulong epochSeconds)
+        {
+            resetAtEpochMilliseconds = epochSeconds * 1000;
+        }
+
+        public void ResetAfter(int milliseconds)
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            ulong epochNowMilliseconds = (ulong)t.TotalMilliseconds;
+
+            resetAtEpochMilliseconds = epochNowMilliseconds + (ulong)milliseconds;
+        }
+
+        public AwaitableDisposable<IDisposable> LockAsync(CancellationToken cancellationToken)
+        {
+            return mutex.LockAsync(cancellationToken);
+        }
+    }
+}
