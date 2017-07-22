bdef3c6f115e3c27e0ba91953efead52d93afe58
Ethan Lafrenais <elafrenais12@gmail.com>
2017-07-22 17:11:31 -0400

    Clean up Gateway file a little bit

--- src/Discore/WebSocket/Net/Gateway.Dispatch.cs
+++ src/Discore/WebSocket/Net/Gateway.Dispatch.cs
@@ -2,7 +2,6 @@
 using Discore.Voice;
 using System;
 using System.Collections.Generic;
-using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,36 +10,6 @@ namespace Discore.WebSocket.Net
 {
     partial class Gateway
     {
-        delegate void DispatchSynchronousCallback(DiscordApiData data);
-        delegate Task DispatchAsynchronousCallback(DiscordApiData data);
-
-        class DispatchCallback
-        {
-            public DispatchSynchronousCallback Synchronous { get; }
-            public DispatchAsynchronousCallback Asynchronous { get; }
-
-            public DispatchCallback(DispatchSynchronousCallback synchronous)
-            {
-                Synchronous = synchronous;
-            }
-
-            public DispatchCallback(DispatchAsynchronousCallback asynchronous)
-            {
-                Asynchronous = asynchronous;
-            }
-        }
-
-        [AttributeUsage(AttributeTargets.Method)]
-        class DispatchEventAttribute : Attribute
-        {
-            public string EventName { get; }
-
-            public DispatchEventAttribute(string eventName)
-            {
-                EventName = eventName;
-            }
-        }
-
         #region Public Events       
         public event EventHandler<DMChannelEventArgs> OnDMChannelCreated;
         public event EventHandler<GuildChannelEventArgs> OnGuildChannelCreated;
@@ -90,39 +59,6 @@ namespace Discore.WebSocket.Net
         public event EventHandler<UserEventArgs> OnUserUpdated;
         public event EventHandler<VoiceStateEventArgs> OnVoiceStateUpdated;
         #endregion
-
-        Dictionary<string, DispatchCallback> dispatchHandlers;
-
-        void InitializeDispatchHandlers()
-        {
-            dispatchHandlers = new Dictionary<string, DispatchCallback>();
-
-            Type taskType = typeof(Task);
-            Type gatewayType = typeof(Gateway);
-            Type dispatchSynchronousType = typeof(DispatchSynchronousCallback);
-            Type dispatchAsynchronousType = typeof(DispatchAsynchronousCallback);
-
-            foreach (MethodInfo method in gatewayType.GetTypeInfo().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic))
-            {
-                DispatchEventAttribute attr = method.GetCustomAttribute<DispatchEventAttribute>();
-                if (attr != null)
-                {
-                    DispatchCallback dispatchCallback;
-                    if (method.ReturnType == taskType)
-                    {
-                        Delegate callback = method.CreateDelegate(dispatchAsynchronousType, this);
-                        dispatchCallback = new DispatchCallback((DispatchAsynchronousCallback)callback);
-                    }
-                    else
-                    {
-                        Delegate callback = method.CreateDelegate(dispatchSynchronousType, this);
-                        dispatchCallback = new DispatchCallback((DispatchSynchronousCallback)callback);
-                    }
-
-                    dispatchHandlers[attr.EventName] = dispatchCallback;
-                }
-            }
-        }
 
         void LogServerTrace(string prefix, DiscordApiData data)
         {
