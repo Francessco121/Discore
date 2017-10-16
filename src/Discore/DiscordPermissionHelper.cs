d09638754e409251ea9b8c645338e4e419b921fc
Unknown <elafrenais12@gmail.com>
2017-10-16 19:29:19 -0400

    Permission checking now matches docs more closely
    Turns out permission checking wasn't really broken. This commit just makes some tweaks to ensure it matches the official documentation as close as possible.
    
    Closes #25.

--- src/Discore/DiscordPermissionHelper.cs
+++ src/Discore/DiscordPermissionHelper.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 
+// Permission logic here is based off of:
+// https://discordapp.com/developers/docs/topics/permissions#permission-hierarchy
+
 namespace Discore
 {
     public static class DiscordPermissionHelper
     {
         /// <summary>
-        /// Gets whether a member has a set of permissions.
+        /// Returns whether the specified member has the given set of permissions.
         /// </summary>
-        /// <param name="permission">The set of permissions to check if the member has.</param>
+        /// <param name="permissions">The set of permissions to check if the member has.</param>
         /// <param name="member">The member to check the permissions of.</param>
         /// <param name="guild">The guild this member is in.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static bool HasPermission(DiscordPermission permission, DiscordGuildMember member, DiscordGuild guild)
+        /// <exception cref="ArgumentException">
+        /// Thrown if <paramref name="member"/> is not in the specified <paramref name="guild"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="member"/> or <paramref name="guild"/> is null.
+        /// </exception>
+        public static bool HasPermission(DiscordPermission permissions, DiscordGuildMember member, DiscordGuild guild)
         {
             if (member == null)
                 throw new ArgumentNullException(nameof(member));
@@ -25,60 +33,70 @@ namespace Discore
             if (member.Id == guild.OwnerId)
                 return true;
 
-            DiscordPermission userPermissions = 0;
+            // Apply @everyone permissions
+            DiscordRole everyoneRole = guild.Roles[guild.Id];
 
-            // Apply guild-member role permissions
+            DiscordPermission userPermissions = everyoneRole.Permissions;
+
+            // Apply permissions for each role the member has
             foreach (Snowflake roleId in member.RoleIds)
             {
-                DiscordRole role;
-                if (guild.Roles.TryGetValue(roleId, out role))
+                if (guild.Roles.TryGetValue(roleId, out DiscordRole role))
                 {
                     userPermissions = userPermissions | role.Permissions;
                 }
             }
 
             // Check for permission
-            return userPermissions.HasFlag(DiscordPermission.Administrator) || userPermissions.HasFlag(permission);
+            return userPermissions.HasFlag(DiscordPermission.Administrator) || userPermissions.HasFlag(permissions);
         }
 
         /// <summary>
-        /// Gets whether a member has a set of permissions, 
+        /// Returns whether the specified member has the given set of permissions
         /// in the context of the specified guild channel.
         /// </summary>
-        /// <param name="permission">The set of permissions to check if the member has.</param>
+        /// <param name="permissions">The set of permissions to check if the member has.</param>
         /// <param name="member">The member to check the permissions of.</param>
         /// <param name="guild">The guild this member is in.</param>
-        /// <param name="forChannel">
+        /// <param name="channel">
         /// The guild channel to check the permissions against (this will take overwrites into account).
         /// </param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static bool HasPermission(DiscordPermission permission, 
-            DiscordGuildMember member, DiscordGuild guild, DiscordGuildChannel forChannel)
+        /// <exception cref="ArgumentException">
+        /// Thrown if <paramref name="member"/> or <paramref name="channel"/> is not in the 
+        /// specified <paramref name="guild"/>
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="member"/>, <paramref name="guild"/>, or <paramref name="channel"/>
+        /// is null.
+        /// </exception>
+        public static bool HasPermission(DiscordPermission permissions, 
+            DiscordGuildMember member, DiscordGuild guild, DiscordGuildChannel channel)
         {
             if (member == null)
                 throw new ArgumentNullException(nameof(member));
             if (guild == null)
                 throw new ArgumentNullException(nameof(guild));
-            if (forChannel == null)
-                throw new ArgumentNullException(nameof(forChannel));
+            if (channel == null)
+                throw new ArgumentNullException(nameof(channel));
 
-            if (forChannel.GuildId != member.GuildId)
+            if (channel.GuildId != member.GuildId)
                 throw new ArgumentException("Guild channel must be in the same guild as this member.");
-            if (forChannel.GuildId != guild.Id)
+            if (channel.GuildId != guild.Id)
                 throw new ArgumentException("Guild channel must be in the specified guild.");
 
             // If owner, everything is true
             if (member.Id == guild.OwnerId)
                 return true;
 
-            // Set default permissions to guild @everyone role permissions
-            DiscordPermission userPermissions = guild.Roles[guild.Id].Permissions;
+            // Apply @everyone permissions
+            DiscordRole everyoneRole = guild.Roles[guild.Id];
 
-            // Apply guild-member role permissions
+            DiscordPermission userPermissions = everyoneRole.Permissions;
+
+            // Apply permissions for each role the member has
             foreach (Snowflake roleId in member.RoleIds)
             {
-                DiscordRole role;
-                if (guild.Roles.TryGetValue(roleId, out role))
+                if (guild.Roles.TryGetValue(roleId, out DiscordRole role))
                 {
                     userPermissions = userPermissions | role.Permissions;
                 }
@@ -90,56 +108,82 @@ namespace Discore
 
             // Apply channel @everyone overwrites
             DiscordOverwrite channelEveryoneOverwrite;
-            if (forChannel.PermissionOverwrites.TryGetValue(guild.Id, out channelEveryoneOverwrite))
+            if (channel.PermissionOverwrites.TryGetValue(guild.Id, out channelEveryoneOverwrite))
             {
-                userPermissions = (userPermissions | channelEveryoneOverwrite.Allow) & (~channelEveryoneOverwrite.Deny);
+                userPermissions = (userPermissions & (~channelEveryoneOverwrite.Deny)) | channelEveryoneOverwrite.Allow;
             }
 
-            // Apply channel-specific overwrites
+            // Apply channel-specific role overwrites
+            DiscordPermission roleOverwriteAllow = 0;
+            DiscordPermission roleOverwriteDeny = 0;
+
             foreach (Snowflake roleId in member.RoleIds)
             {
                 DiscordOverwrite overwrite;
-                if (forChannel.PermissionOverwrites.TryGetValue(roleId, out overwrite))
+                if (channel.PermissionOverwrites.TryGetValue(roleId, out overwrite))
                 {
-                    userPermissions = (userPermissions | overwrite.Allow) & (~overwrite.Deny);
+                    roleOverwriteAllow = roleOverwriteAllow | overwrite.Allow;
+                    roleOverwriteDeny = roleOverwriteDeny | overwrite.Deny;
                 }
             }
 
+            userPermissions = (userPermissions & (~roleOverwriteDeny)) | roleOverwriteAllow;
+
             // Apply channel-specific member overwrite for this channel
             DiscordOverwrite memberOverwrite;
-            if (forChannel.PermissionOverwrites.TryGetValue(member.Id, out memberOverwrite))
+            if (channel.PermissionOverwrites.TryGetValue(member.Id, out memberOverwrite))
             {
-                userPermissions = userPermissions & (~memberOverwrite.Deny) | memberOverwrite.Allow;
+                userPermissions = (userPermissions & (~memberOverwrite.Deny)) | memberOverwrite.Allow;
             }
 
             // Check for correct permissions
-            return userPermissions.HasFlag(DiscordPermission.Administrator) | userPermissions.HasFlag(permission);
+            return userPermissions.HasFlag(DiscordPermission.Administrator) | userPermissions.HasFlag(permissions);
         }
 
         /// <summary>
-        /// Checks whether a member has a set of permissions,
-        /// if they don't a <see cref="DiscordPermissionException"/> is thrown with details.
+        /// Checks whether the specified member has the given set of permissions.
+        /// If they don't, a <see cref="DiscordPermissionException"/> is thrown with details.
         /// </summary>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="DiscordPermissionException"></exception>
-        public static void AssertPermission(DiscordPermission permission,
+        /// <param name="permissions">The set of permissions to check if the member has.</param>
+        /// <param name="member">The member to check the permissions of.</param>
+        /// <param name="guild">The guild this member is in.</param>
+        /// <exception cref="ArgumentException">
+        /// Thrown if <paramref name="member"/> is not in the specified <paramref name="guild"/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="member"/> or <paramref name="guild"/> is null.
+        /// </exception>
+        public static void AssertPermission(DiscordPermission permissions,
             DiscordGuildMember member, DiscordGuild guild)
         {
-            if (!HasPermission(permission, member, guild))
-                throw new DiscordPermissionException(member, guild, permission);
+            if (!HasPermission(permissions, member, guild))
+                throw new DiscordPermissionException(member, guild, permissions);
         }
 
         /// <summary>
-        /// Checks whether a member has a set of permissions in the context of a guild channel,
-        /// if they don't a <see cref="DiscordPermissionException"/> is thrown with details.
+        /// Checks whether the specified member has the given set of permissions 
+        /// in the context of the specified guild channel.
+        /// If they don't, a <see cref="DiscordPermissionException"/> is thrown with details.
         /// </summary>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="DiscordPermissionException"></exception>
-        public static void AssertPermission(DiscordPermission permission,
-            DiscordGuildMember member, DiscordGuild guild, DiscordGuildChannel forChannel)
+        /// <param name="permissions">The set of permissions to check if the member has.</param>
+        /// <param name="member">The member to check the permissions of.</param>
+        /// <param name="guild">The guild this member is in.</param>
+        /// <param name="channel">
+        /// The guild channel to check the permissions against (this will take overwrites into account).
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown if <paramref name="member"/> or <paramref name="channel"/> is not in the 
+        /// specified <paramref name="guild"/>
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="member"/>, <paramref name="guild"/>, or <paramref name="channel"/>
+        /// is null.
+        /// </exception>
+        public static void AssertPermission(DiscordPermission permissions,
+            DiscordGuildMember member, DiscordGuild guild, DiscordGuildChannel channel)
         {
-            if (!HasPermission(permission, member, guild, forChannel))
-                throw new DiscordPermissionException(member, guild, forChannel, permission);
+            if (!HasPermission(permissions, member, guild, channel))
+                throw new DiscordPermissionException(member, guild, channel, permissions);
         }
     }
 }
